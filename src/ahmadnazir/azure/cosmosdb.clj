f7576ee81e11ebbc918df6fb68b5b9d7d4dfee25
Ahmad Nazir Raja <ahmadnazir@gmail.com>
2020-02-16 12:14:37 +0100

    Moved cosmos db functionality to separate namespace

---
+++ src/ahmadnazir/azure/cosmosdb.clj
@@ -0,0 +1,52 @@
+(ns ahmadnazir.azure.cosmosdb
+  (:require [cheshire.core :as json])
+  (:import [com.azure.cosmos CosmosClientBuilder]
+           [com.azure.cosmos FeedOptions])
+  )
+
+(defn client [url key]
+  "Get the client"
+  (-> (CosmosClientBuilder. )
+      (.setEndpoint url)
+      (.setKey key)
+      (.buildClient)
+      ))
+
+(defn container [database container client]
+  "Get the reference to the container"
+  (-> client
+      (.getDatabase database)
+      (.getContainer container)
+      ))
+
+(def feed-options
+  "Get the default feed options"
+  (-> (FeedOptions.)
+      (.maxItemCount (int 10))
+      (.setEnableCrossPartitionQuery true)
+      ))
+
+(defn query [container s]
+  (-> container
+      (.queryItems s feed-options)
+      ))
+
+(defn page [n iterator]
+  (->> iterator
+       iterator-seq
+       (#(nth %1 n))
+       .getResults
+       ;; .size
+       ))
+
+
+(defn json-prettify [x]
+  (-> x
+      json/parse-string
+      (json/generate-string {:pretty true})))
+
+(defn contents [result]
+  (letfn [(to-json [x] (.toJson x))]
+    (->> result
+         to-json
+         json-prettify)))
--- src/ahmadnazir/azure_storage_client.clj
+++ src/ahmadnazir/azure_storage_client.clj
@@ -1,68 +1,23 @@
 (ns ahmadnazir.azure-storage-client
-  (:import [com.azure.cosmos CosmosClientBuilder]
-           [com.azure.cosmos FeedOptions])
   (:require
-   [environ.core :refer [env]]
-   ;; [ahmadnazir.env :refer [env]]
-   [cheshire.core :as json]
+   ;; [environ.core :refer [env]]
+   [ahmadnazir.env :refer [env]]
+   [ahmadnazir.azure.cosmosdb :as cosmosdb]
    )
   (:gen-class))
 
 ;; (ns-unmap 'ahmadnazir.azure-storage-client 'env)
 
-(defn client []
-  "Get the client"
-  (-> (CosmosClientBuilder. )
-      (.setEndpoint (env :azure-cosmosdb-url))
-      (.setKey (env :azure-cosmosdb-key))
-      (.buildClient)
-      ))
-
 (def container
-  "Get the reference to the container"
-  (-> (client)
-      (.getDatabase (env :azure-cosmosdb-database))
-      (.getContainer (env :azure-cosmosdb-container))
-      ))
-
-(def feed-options
-  "Get the default feed options"
-  (-> (FeedOptions.)
-      (.maxItemCount (int 10))
-      (.setEnableCrossPartitionQuery true)
-      ))
-
-(defn exec [query]
-  (-> container
-      (.queryItems query feed-options)
-      ))
-
-(defn page [n iterator]
-  (->> iterator
-       iterator-seq
-       (#(nth %1 n))
-       .getResults
-       ;; .size
-       ))
-
-
-(defn json-prettify [x]
-  (-> x
-       json/parse-string
-       (json/generate-string {:pretty true})))
-
-(defn contents [result]
-  (letfn [(to-json [x] (.toJson x))]
-    (->> result
-         to-json
-         json-prettify)))
+  (->> (cosmosdb/client (env :azure-cosmosdb-url) (env :azure-cosmosdb-key))
+       (cosmosdb/container (env :azure-cosmosdb-database) (env :azure-cosmosdb-container))))
 
 (defn -main
   "Execute the query"
   [& args]
   (->> args
       first
-      exec
+      (cosmosdb/query container)
       (page 0)
       (map contents)
       (map println) dorun ;; map needs eager evaluation
@@ -72,4 +27,3 @@
 ;; (->>  "SELECT * FROM c WHERE c.partitionKey = \"b1000dd0-a811-43e1-856b-47c15cb9ee7c\""
 ;;       -main
 ;;       time)
-
