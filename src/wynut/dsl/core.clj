347de466c3272435ebfa559b5efb7680b4d5bfe4
Ahmad Nazir Raja <ahmadnazir@gmail.com>
2020-03-04 17:40:14 +0100

    Basic DSL which is a subset of KQL

--- src/wynut/dsl/core.clj
+++ src/wynut/dsl/core.clj
@@ -13,10 +13,4 @@
 (defn evaluate
   "Convert parse tree to abstract syntax tree"
   [parsed]
-  (match parsed [:EXPR [:NUMBER l] [:OP op] [:NUMBER r]] (+ (read-string l) (read-string r) ) ;; hardcode the operator
-         :else  {:error (format "Matching rules incomplete for: %s" parsed)}
-         ))
-
-(->> "1 + 2"
-     parse
-     evaluate)
+  nil)
