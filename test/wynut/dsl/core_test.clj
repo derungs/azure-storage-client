d9ed06797409e1823aa152099714b5afeae4f660
Ahmad Nazir Raja <ahmadnazir@gmail.com>
2020-03-10 23:09:21 +0100

    Support for quoted strings

--- test/wynut/dsl/core_test.clj
+++ test/wynut/dsl/core_test.clj
@@ -1,6 +1,13 @@
 (ns wynut.dsl.core-test
   (:require [wynut.dsl.core :as sut]
-            [clojure.test :refer [deftest testing is]]))
+            [clojure.test :refer [deftest testing is]]
+            [clojure.string :as s]
+            ))
+
+(defn stringify [s]
+  "Helper function to create quoted strings while escaping quotes. Only works
+  with double quotes."
+  (str "\"" (s/replace s #"\"{1}" "\\\\\"") "\""))
 
 (deftest test-simple-expression
   (testing "
@@ -8,8 +15,17 @@ Transform: c | where partitionKey == 'b1000dd0-a811-43e1-856b-47c15cb9ee7c'
 to:        SELECT * FROM c WHERE c.partitionKey = 'b1000dd0-a811-43e1-856b-47c15cb9ee7c'
 "
     (is (=
-         (sut/parse "c | where partitionKey == abc")
+         (sut/parse (str "c | where partitionKey == " (stringify "abc")))
          [:EXPRS
-          [:EXPR [:TABLE [:string "c"]]]
-          [:EXPR [:WHERE [:string "partitionKey"] [:string "abc"]]]]))))
+          [:EXPR [:TABLE [:keyword "c"]]]
+          [:EXPR [:WHERE [:keyword "partitionKey"] [:string "abc"]]]]))))
+
+
+(deftest parse-escaped-string
+  (testing "Escaped string"
+    (is (=
+         (sut/parse (str "c | where partitionKey == " (stringify "ab\"c")) )
+         [:EXPRS
+          [:EXPR [:TABLE [:keyword "c"]]]
+          [:EXPR [:WHERE [:keyword "partitionKey"] [:string "ab" "\\\"" "c"]]]]))))
 
