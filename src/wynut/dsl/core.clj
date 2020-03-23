fc0959a3c456db9f6d5c8f9b7d098beb0dd12488
Andrea Aczel <aczelandi@gmail.com>
2020-03-23 16:45:40 +0100

    :string-ci flag controls whether or not the language is case sensitive

--- src/wynut/dsl/core.clj
+++ src/wynut/dsl/core.clj
@@ -2,14 +2,17 @@
   (:require
    [instaparse.core :as insta]))
 
-(def parse (->> (System/getProperty "user.dir")
-                (format "%s/src/wynut/dsl/grammar.bnf" )
-                slurp
-                insta/parser
-                ))
+(defn get-grammar [] 
+  (->> (System/getProperty "user.dir")
+       (format "%s/src/wynut/dsl/grammar.bnf" )
+       slurp
+       ))
+
+(defn parse [query] 
+ (let [grammar (get-grammar)]
+        ((insta/parser grammar :string-ci true) query)))
 
 (defn evaluate
   "Convert parse tree to abstract syntax tree"
   [parsed]
   (throw (Exception. "Generic evaluation of parse tree is not available. Implementation should be specific to the different storage modules.")))
-
