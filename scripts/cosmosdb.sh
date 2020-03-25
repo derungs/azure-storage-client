0901874e59ded99bc35b15b8e90e74ff6ba04b1c
Ahmad Nazir Raja <ahmadnazir@gmail.com>
2020-03-25 03:31:18 +0100

    Code formatting check for CI pipeline

--- scripts/cosmosdb.sh
+++ scripts/cosmosdb.sh
@@ -4,4 +4,4 @@ PORT=$1
 TYPE=$2
 QUERY="$3"
 
-curl -G -H Accept\:\ application/json -XGET http\://localhost\:$PORT/api/cosmosdb --data-urlencode "type=$TYPE" --data-urlencode "query=$QUERY"
+curl -sG -H Accept\:\ application/json -XGET http\://localhost\:$PORT/api/cosmosdb --data-urlencode "type=$TYPE" --data-urlencode "query=$QUERY"
---
+++ scripts/format.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+operation=$1
+
+case $operation in
+    fix|check) ;;
+    *)         echo "Usage: format.sh [check|fix]" && exit 1;;
+esac
+
+touch project.clj # see: https://github.com/weavejester/cljfmt/issues/192
+clojure -Sdeps '{:deps {cljfmt {:mvn/version "0.6.4"}}}' -m cljfmt.main $operation
